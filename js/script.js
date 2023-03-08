53b6502fb7521ced9680f4fe057d2e98e130d4b2
CaitlynHagin <caitlyn32h@gmail.com>
2023-03-08 17:59:36 +0000

    parameters

--- js/script.js
+++ js/script.js
@@ -33,3 +33,27 @@ function turns(x) {
 turns(7)
 
 
+
+// build a function that takes 3 paraments (of Numbers) if the parameter numbers are between 50 and 100 console.log parameter and true
+//comment above the code is what it's doing
+
+//declaring function with 3 parameters (a,b,c), they represent possible numbers the user would put in
+function Checker(a,b,c) {
+
+// for loop, declared index numbers, i equal 50, i being less than 100, i++ is adding on
+ for (let i=50; i<100; i++) {
+    //console logging the index 
+//  console.log (i)
+// asking the if statement to be equal to the index, or statement
+  if  (a === i || b === i || c === i) {
+    console.log ("you win", i)
+    //displaying in the console instead if it fits the i statments, using the i statement
+}
+else {
+    //this is an else statement
+    console.log ("wrong number")
+}
+}
+}
+//this is calling the function
+Checker (25,67,89)
